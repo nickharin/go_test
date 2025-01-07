beb6c77b3ab545e388ef621ee46a5d7bdd8aa171
nickharin <kharinnick@gmail.com>
2025-01-07 14:20:39 +0500

    fix ish

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
+
+func init() {
+	if runtime.GOARCH == "386" {
+		runtime.GOMAXPROCS(1)
+	}
+}
 
 func main() {
 	fmt.Print("Hello World")
